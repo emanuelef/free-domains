720546a27f324b4cd29fe6ba475b8123ce4519d4
Emanuele Fumagalli <emanuelef@users.noreply.github.com>
2024-06-01 08:21:14 +0100

    Create awesome-rust-stats.1bt.uk.js

---
+++ domains/awesome-rust-stats.1bt.uk.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Awesome Rust Stats',
+  domain: '1bt.uk',
+  subdomain: 'awesome-rust-stats',
+  owner: {
+    repo: 'https://github.com/emanuelef/awesome-rust-repo-stats',
+    email: 'efumagal@proton.me',
+  },
+  record: {
+    CNAME: 'emanuelef.github.io',
+  },
+  proxy: false,
+})
